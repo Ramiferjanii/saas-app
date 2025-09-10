1b6bfc3ddb056bdaa5da5322bd97013bc3e260c1
ramiferjani <ramiferjani.20.@gmail.com>
2025-09-11 00:12:10 +0100

    Companions CARD aDD

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Button } from '@/components/ui/button' 
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import CompanionsCard from '@/components/CompanionsCard'
+
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline ' >Welcom to my page </h1>
-      <Button  > let's go  </Button>
-    </div>
+    <main>
+      <h1  >Popular companions  </h1>
+      <section className='home-section' >
+        <CompanionsCard id="123" name='Naura the brainy Explorer' topic="Neural Netwark of the Brain " subject="science" duration={45}  color="#ffda6e" />
+        <CompanionsCard id="34" name='Country the Number Wizard ' topic=" Derivations & Integrals " subject="Math" duration={30}  color="#e5d0ff" />
+        <CompanionsCard  id="23" name='Verba the Vocabulary Builder' topic="Language " subject="Enlish Literature" duration={45}  color="#BDE7FF"/>
+      </section>
+
+      <section className='home-section' >
+        <CompanionsList />
+        <CTA />
+      </section>
+  
+      </main>
   )
 }
 
